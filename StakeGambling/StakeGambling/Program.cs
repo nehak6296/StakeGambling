26b3472efc22fd3fb6dd2c2fd02ead56477da0f2
nehak6296 <nehak6296@gmail.com>
2021-03-10 14:01:11 +0530

    [Neha]Added Welcome Message

---
+++ StakeGambling/StakeGambling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StakeGambling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stake Gambling Problem");
+        }
+    }
+}
