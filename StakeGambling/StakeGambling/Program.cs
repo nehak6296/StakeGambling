970954167d6c1deb769aff72dec6fb6d413deefb
nehak6296 <nehak6296@gmail.com>
2021-03-10 14:08:06 +0530

    [Neha]Added Variables

--- StakeGambling/StakeGambling/Program.cs
+++ StakeGambling/StakeGambling/Program.cs
@@ -4,9 +4,11 @@ namespace StakeGambling
 {
     class Program
     {
+        public const int STAKE = 100;
+        public const int BET = 1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Stake Gambling Problem");
+            
         }
     }
 }
