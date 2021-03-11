f740ab1f9ed5ded9507a4d2351fe11bc19662987
nehak6296 <nehak6296@gmail.com>
2021-03-11 13:12:33 +0530

    [Neha]Add Checking condition

--- StakeGambling/StakeGambling/Program.cs
+++ StakeGambling/StakeGambling/Program.cs
@@ -4,31 +4,47 @@ namespace StakeGambling
 {
     class Program
     {
-        public const int STAKE = 100;
+        public int STAKE = 100;
         public const int BET = 1;
         public const int IS_WIN = 1;
-        public const int IS_lost = 0;        
+        public const int IS_lost = 0;
 
         public void Play()
         {
             Random random = new Random();
             int check = random.Next(0, 2);
-                        
+            int countWin = 0;
+            int countLose = 0;
+
+            for (int day = 0; day <= 20; day++)
+            {
+
                 if (check == IS_WIN)
 
                 {
                     Console.WriteLine("You Win");
+                    countWin++;
+                    STAKE++;
                 }
                 else
                 {
                     Console.WriteLine("You Lose");
-                    
+                    countLose++;
+                    STAKE--;
+
                 }
             }
-            static void Main(string[] args)
-        {
-            Program program  = new Program();
-            program.Play();
+             Console.WriteLine("Number of times win:"+countWin);
+            Console.WriteLine("Number of times lost:" + countLose);           
         }
+       
+
+    }
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Play();
     }
 }
+
+
